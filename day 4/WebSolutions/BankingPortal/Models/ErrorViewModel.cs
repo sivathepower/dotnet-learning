68309d4085ad5081613220b356f2821aeb26531a
sivasagarg <sivasagarg@nedbank.co.za>
2025-09-03 18:46:07 +0200

    day 4 completed

---
+++ day 4/WebSolutions/BankingPortal/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace BankingPortal.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
